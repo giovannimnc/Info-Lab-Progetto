d1b1166fb7e1714b097a48f71232605c5878f7ef
giovannimnc <giovanni.menicucci@outlook.it>
2026-06-06 23:10:57 +0200

    Aggiunte al documento; funzioni vuote in discretizzazione.cpp; aggiunto .gitignore

---
+++ codice_cpp/discretizzazione.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+struct nodo {
+    int n;              // indice progressivo
+    int i,j;            // indici sulla griglia
+    double x,y;         // coordinate spaziali
+};
+
+void GeneraGriglia(const int N, vector<nodo>& nodi, vector<vector<int>>& griglia) {
+
+};
+
+void EsportaPuntiInterni(const int N, const vector<nodo>& nodi) {
+
+};
+
+void GeneraEsportaListaAdiacenza(const int N, const vector<vector<int>>& griglia, vector<vector<int>>& lista_adiacenza) {
+
+};
+
+
+
+int main() {
+    const int N = 32;
+
+    return 0;
+}
