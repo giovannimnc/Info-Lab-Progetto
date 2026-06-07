44b0ac576e1b98030b562467cd75247025cac3c5
giovannimnc <giovanni.menicucci@outlook.it>
2026-06-07 22:37:42 +0200

    Scritto codice discretizzazione, aggiornato doc, generato file connectivity e coords per i vari N

--- codice_cpp/discretizzazione.cpp
+++ codice_cpp/discretizzazione.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
@@ -11,21 +13,86 @@ struct nodo {
 };
 
 void GeneraGriglia(const int N, vector<nodo>& nodi, vector<vector<int>>& griglia) {
+    const double h = 1.0 / (N+1);       // passo della griglia
+    int ind_progr = 0;                  // indice n che numera i nodi
 
+    for (int i = 1; i<=N; i++) {        // ciclo solo sui nodi interni
+        for (int j = 1; j<=N; j++) {
+            nodo nodo_corrente;
+            nodo_corrente.n = ind_progr;
+            nodo_corrente.i = i;
+            nodo_corrente.j = j;
+            nodo_corrente.x = i*h;
+            nodo_corrente.y = j*h;
+
+            nodi.push_back(nodo_corrente);  // aggiunto in fondo al vettore dei nodi 
+            griglia[i][j] = ind_progr;
+
+            ind_progr++;
+
+        }
+    }
 };
 
 void EsportaPuntiInterni(const int N, const vector<nodo>& nodi) {
+    string nome_file = "../dati/coords" + to_string(N) + ".txt";    // "../dati" torna indietro alla cartella radice
+    ofstream file_out(nome_file);
 
+    for (const auto& m : nodi) {
+        file_out << m.n << " " << m.i << " " << m.j << " " << m.x << " " << m.y << "\n";
+
+    }
+
+    file_out.close();
 };
 
-void GeneraEsportaListaAdiacenza(const int N, const vector<vector<int>>& griglia, vector<vector<int>>& lista_adiacenza) {
+void GeneraEsportaGrafo(const int N, const vector<vector<int>>& griglia, vector<vector<int>>& lista_adiacenza) {
+    string nome_file = "../dati/connectivity" + to_string(N) + ".txt";    
+    ofstream file_out(nome_file);
 
+    int e = 0;      // indice progressivo che numera gli archi
+
+    for (int i = 1; i<=N; i++) {
+        for (int j = 1; j<=N; j++) {
+            auto a = griglia[i][j];
+            auto b = griglia[i+1][j];   // vicino a destra
+            auto c = griglia[i][j+1];   // vicino in alto
+
+            if (b != -1) {          // se il nodo (i+1,j) non è di bordo, aggiungo alla lista di adiacenza e scrivo l'arco su file
+                lista_adiacenza[a].push_back(b);
+                lista_adiacenza[b].push_back(a);
+
+                file_out << e << " " << a << " " << b << "\n";
+                e++;
+            }
+
+            if (c != -1) {
+                lista_adiacenza[a].push_back(c);
+                lista_adiacenza[c].push_back(a);
+
+                file_out << e << " " << a << " " << c << "\n";
+                e++;
+            }
+        }
+    }
+
+    file_out.close();
 };
 
 
 
 int main() {
-    const int N = 32;
+    const int N = 1024;
+
+    vector<nodo> nodi;
+    vector<vector<int>> griglia(N + 2, vector<int>(N + 2, -1));
+    vector<vector<int>> lista_adiacenza(N * N);
+
+    // GeneraGriglia(N, nodi, griglia);
+    // EsportaPuntiInterni(N, nodi);
+    // GeneraEsportaGrafo(N, griglia, lista_adiacenza);
+
+    cout <<"aa"<<endl;
 
     return 0;
 }
