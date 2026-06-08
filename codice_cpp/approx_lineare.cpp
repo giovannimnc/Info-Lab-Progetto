ee844c88eff8fa89579566c9da23414d69ddf8d1
giovannimnc <giovanni.menicucci@outlook.it>
2026-06-08 23:34:21 +0200

    Aggiunto al doc il piano per il modulo di approx lineare

---
+++ codice_cpp/approx_lineare.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    const int N = 4;
+
+    cout << "ok"<<endl;
+}
